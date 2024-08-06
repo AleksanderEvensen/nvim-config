f35c3997f6ef34a38ee05b89f890e646affde3c8
Aleksander Evensen <ahse03@gmail.com>
2024-08-07 00:22:28 +0200

    feat(lsp): Added typescript and zig support

--- lua/ahse/languageServers.lua
+++ lua/ahse/languageServers.lua
@@ -1,11 +1,15 @@
 local lsp_zero = require('lsp-zero');
 local mason = require('mason');
 local mason_lspc = require('mason-lspconfig');
+local cmp = require('cmp');
 
 lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({buffer = bufnr}) 
-    vim.keymap.set('n', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 end)
+
+vim.diagnostic.config({
+  update_in_insert = true
+})
 
 mason.setup()
 mason_lspc.setup({
@@ -15,8 +19,34 @@ mason_lspc.setup({
             require('lspconfig')[server_name].setup()
         end,
 
+        rust_analyzer = function()
+            require('lspconfig').rust_analyzer.setup({})
+        end,
+
+        gopls = function()
+            require('lspconfig').gopls.setup({})
+        end,
+
         lua_ls = function()
             require('lspconfig').lua_ls.setup({ })
-        end
+        end,
+
+        zls = function()
+            require('lspconfig').zls.setup({ })
+        end,
+
+        tsserver = function()
+            require('lspconfig').tsserver.setup({ });
+        end,
     }
 })
+
+cmp.setup({
+    sources = { { name = "nvim_lsp" } },
+    mapping = {
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true })
+    },
+})
+
