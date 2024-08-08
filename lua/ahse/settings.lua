eabced3abff1b70f83bc3d04cb4ce1fba6a9506f
Aleksander Evensen <ahse03@gmail.com>
2024-08-08 19:08:56 +0200

    fix(settings): Fixed HOME env null on windows

--- lua/ahse/settings.lua
+++ lua/ahse/settings.lua
@@ -1,10 +1,31 @@
+function getHomeDir()
+    local ENV_HOME = os.getenv("HOME");
+
+    -- Linux and MacOS
+    if type(ENV_HOME) == "string" then
+        return ENV_HOME
+    end
+
+	local ENV_HOMEDRIVE = os.getenv("HOMEDRIVE");
+    local ENV_HOMEPATH = os.getenv("HOMEPATH");
+
+    -- Windows
+    if type(ENV_HOMEDRIVE) == "string" and type(ENV_HOMEPATH) == "string" then 
+        return string.gsub(ENV_HOMEDRIVE..ENV_HOMEPATH,"\\", "/")
+    end
+
+
+    return nil
+end
+
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = truei
 
 vim.opt.smartindent = true
 
@@ -12,7 +33,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = getHomeDir() .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
