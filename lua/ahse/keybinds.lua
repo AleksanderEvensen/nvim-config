3aa87ed28044dc461b553e88bf2734704d068d12
Aleksander Evensen <ahse03@gmail.com>
2024-08-09 16:22:28 +0200

    chore: Added keybind descriptions for which-key

--- lua/ahse/keybinds.lua
+++ lua/ahse/keybinds.lua
@@ -1,19 +1,39 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local wk = require('which-key');
+local telescope = require('telescope.builtin');
+---@param mode string|table
+---@param key string
+---@param exec string|function
+---@param options table
+local function mapping(mode, key, exec, options)
+    local key_data = { key, exec };
 
--- Format Buffer
-vim.keymap.set("n", "<leader>fb", function()
-    vim.lsp.buf.format({  })
-end)
+    key_data.mode = mode
 
+    -- Merge tables
+    if type(options) == "table" then
+        for k, v in pairs(options) do
+            key_data[k] = v
+        end
+    end
 
--- Telescope Keybinds
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', telescope.find_files, {})
-vim.keymap.set('n', '<leader>pg', telescope.live_grep, {})
-vim.keymap.set('n', '<C-p>', telescope.git_files, {})
+    return key_data
+end
 
--- Undotree keybinds
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
+wk.add({
+    -- Open Netrw
+    mapping("n", "<leader>pv", vim.cmd.Ex, { desc = "Open Netrw" }),
 
--- LazyGit
-vim.keymap.set('n', '<leader>lg', vim.cmd.LazyGit, {})
+    -- Format Buffer
+    mapping("n", "<leader>fb", vim.lsp.buf.format, { desc = "Format the current buffer using the LSP" }),
+
+    -- Telescope Keybinds
+    mapping('n', '<leader>pf', telescope.find_files, { desc = "Find files in the cwd using Telescope" }),
+    mapping('n', '<leader>pg', telescope.live_grep, { desc = "Find in files using ripgrep and Telescope" }),
+    mapping('n', '<C-p>', telescope.git_files, { desc = "Find files tracked by git using Telescope" }),
+
+    -- Undotree keybinds
+    mapping('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = "Open undotree", icon = "󰕌" }),
+
+    -- LazyGit
+    mapping('n', '<leader>lg', vim.cmd.LazyGit, { desc = "Open lazygit in the current repo" }),
+});
